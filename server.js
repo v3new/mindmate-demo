d0f5d9ef5b33149caac0944d3f44bea0771595d9
Sasha Verenuk <verenuk@gmail.com>
2025-06-22 20:53:35 +0300

    add products to loyalty

--- server.js
+++ server.js
@@ -82,7 +82,7 @@ app.post('/api/chat', async (req, res) => {
           return `${h.date}: ${h.event} (${h.points})`;
         }
         const sign = h.change > 0 ? '+' : '';
-        return `${h.date}: ${h.reason} (${sign}${h.change})`;
+        return `${h.date}: ${h.reason} (${sign}${h.change}: ${h.products.map(p => p.name).join(', ')})`;
       })
       .join('\n');
     systemPrompt =
